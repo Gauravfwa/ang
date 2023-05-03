6330918bbc84862d2b0d2797f257918fa310ef09
RoliGautam21 <roligautam320@gmail.com>
2023-05-03 15:58:43 +0530

    two way data binding done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,12 +8,19 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Gaurav'
-  };
-  user = {
-    type: 'Admin',
-    password: 'dh@hjhfgh$hjg6'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Gaurav',
+  //   course:'bca'
+  // };
+  // user = {
+  //   type: 'Admin',
+  //   password: 'dh@hjhfgh$hjg6'
+  // };
+  heroes=HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero"
+
+
+export const HEROES: Hero[] = [
+    {id:5, name:'gaurav',course:'bca'},
+    {id:6,name:'saurav',course:'mca'},
+    {id:7,name:'asim',course:'Btech'},
+    {id:8,name:'garry',course:'mca'},
+
+]
